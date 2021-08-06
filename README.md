5a88fc6947824761ec89343c50375b26f539d825
Peter C. Norton <spacey-github.com@ssr.com>
2021-08-06 09:09:44 -0400

    It's a bot. It's like many others.

---
+++ README.md
@@ -0,0 +1,25 @@
+# errbot-weather
+
+This is a plugin for errbot which stores responses to questions. 
+
+## install
+- setup & use virtualenv
+- pip install -r requirements.txt
+
+## usage
+- see !help
+- !rg add <trigger string> <reply string>
+- !rg del <trigger string>
+- !rg <trigger string>
+- !rg list
+
+
+## example
+
+```
+!rg reply guy
+
+>>>
+
+https://www.urbandictionary.com/define.php?term=reply%20guy
+```
---
+++ replyguy.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from errbot import BotPlugin, botcmd
+from urllib import error
+import json
+import requests
+
+
+class ReplyGuy(BotPlugin):
+    """Stores replies to questions (links etc)
+
+    """
+    def initialize_persistence(self, key, empty):
+        """Conditinonally initialize an empty
+        container for persistence, but only if it
+        is not currently bound"""
+        self.log.info(f"Checking to see if {key} is available")
+        if not self.get(key):
+            self.log.info(f"Initializing {key} is available")
+            self[key] = empty
+
+
+    def get_trigger(self, word):
+        """Lookup key."""
+        with self.mutable('REPLY_GUY_TRIGGERS') as triggers:
+            return triggers.get(word, "Seriously, are we not still doing phrasing?")
+
+    def set_trigger(self, word, reply):
+        """Lookup key."""
+        with self.mutable('REPLY_GUY_TRIGGERS') as triggers:
+            former_reply = get_trigger(word)
+            triggers.set(word, reply)
+            if former_reply:
+                return f"Set the phrase {word} to {reply} (was {former_reply})"
+            return f"Set the phrase {word} to {reply}"
+
+
+    @botcmd(split_args_with=None)
+    def rg(self, cmd, trigger=None, reply=None):
+        """
+        (!rg add <trigger> <reply>) add a trigger phrase
+        (!rg list <trigger>) list trigger phrases
+        (!rg del <trigger>) get rid of a trigger phrase
+        (!rg <trigger>) reply to the trigger phrase
+        """
+        self.log.iunfo(f"Got this trigger: {msg} {args}")
+        self.initialize_persistence(key, dict())
+
+        if cmd not in ('add', 'list', 'del'):
+            return get_trigger(cmd)
+
+        if cmd == 'add' and trigger is not None and reply is not None:
+            return set_trigger(trigger, reply)
+
+        if cmd == 'del' and trigger is not None:
+            return "Elaborate voicemail prank"
+
+        if cmd == 'list':
+            return "Elaborate voicemail prank"
+
+        return "I didn't know what to do with whatever that was"
